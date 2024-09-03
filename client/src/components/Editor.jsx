8ad7e64a100ac43433c8b728c7f214503b26d701
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-09-04 02:47:46 +0500

    fixing height issues

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,50 +1,53 @@
 import { useRef, useState } from "react";
-import {Editor as Edit} from "@monaco-editor/react";
-import {DNA} from "react-loader-spinner";
+import { Editor as Edit } from "@monaco-editor/react";
+import { DNA } from "react-loader-spinner";
 import { executeCode } from "../../output_api";
 
-function Editor(){
-    const [output, setOutput] = useState("");
-    const [language, setLanguage] = useState("javascript");
-    const editRef = useRef();
+function Editor() {
+	const [output, setOutput] = useState("");
+	const [language, setLanguage] = useState("javascript");
+	const editRef = useRef();
 
-    function handleEditorDidMount(editor){
-        editRef.current = editor;
-    }
+	function handleEditorDidMount(editor) {
+		editRef.current = editor;
+	}
 
-    async function runCode(){
-        const code = editRef.current.getValue();
-        if(!code) return;
-        try{
-            const {run: result} = await executeCode(code);
-            setOutput(result);
-            console.log(output);
-        }
-        catch(err){
+	async function runCode() {
+		const code = editRef.current.getValue();
+		if (!code) return;
+		try {
+			const result = await executeCode(code);
+            console.log(result);
+			// const { run: result } = await executeCode(code);
+			// setOutput(result);
+			// console.log(output);
+		} catch (err) {}
+	}
 
-        }
-    }
-
-    return (
-<>
-            <Edit
-                className="custom"
-                height="90vh"
-                defaultLanguage={language}
-                theme="vs-dark"
-                onMount={handleEditorDidMount}
-                loading={<DNA
-                    visible={true}
-                    height="80"
-                    width="80"
-                    ariaLabel="dna-loading"
-                    wrapperStyle={{}}
-                    wrapperClass="dna-wrapper"
-                    />}
-          />
-          <button className="btn" onClick={runCode}>Run Code</button>
-</>
-    )
-};
+	return (
+		<>
+			<Edit
+				className="custom"
+				height="90vh"
+				defaultLanguage={language}
+				theme="vs-dark"
+				onMount={handleEditorDidMount}
+				loading={
+					<DNA
+						visible={true}
+						height="80"
+						width="80"
+						ariaLabel="dna-loading"
+						wrapperStyle={{}}
+						wrapperClass="dna-wrapper"
+					/>
+				}
+			/>
+			<button className="btn" onClick={runCode}>
+				Run Code
+			</button>
+		</>
+	);
+}
 
 export default Editor;
