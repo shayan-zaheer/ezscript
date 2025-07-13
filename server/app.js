6be1f18b97541ef3268b0e356d56df6b7a74eaf8
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-07-14 02:53:20 +0500

    [FIX]: Added backend url

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const ACTIONS = require("./actions");
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["https://ezscript.onrender.com", "http://localhost:5173"],
         methods: ["GET", "POST"],
         credentials: true,
     },
